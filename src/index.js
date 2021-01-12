997a01ecfcc17c64f130ea2d77d30a2ab2aa412d
Blaire Baker <bakerblaire@gmail.com>
2021-01-12 15:23:53 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('#create-task-form').addEventListener('submit', submitHandler)
 });
+
+function submitHandler(event) {
+  event.preventDefault()
+  let taskDescription = event.target.description.value 
+  let taskDuration = event.target.duration.value
+  let taskPriority = event.target.querySelector('select').value
+  addTask(taskDescription, taskDuration, taskPriority)
+}
+
+function addTask(description, duration, priority) {
+  let li = document.createElement('li')
+  li.textContent = `${description} - ${duration}`
+  if (priority === "low") {
+    li.style.color = "green"
+  } else if (priority === "medium") {
+    li.style.color = "yellow"
+  } else {
+    li.style.color = "red"
+  }
+
+  let button = document.createElement('button')
+  button.textContent = "X"
+  button["data-description"] = description
+  button.addEventListener('click', deleteTask)
+
+  let ul = document.querySelector('ul')
+  li.appendChild(button)
+  ul.appendChild(li)
+
+  document.querySelector('#create-task-form').reset()
+}
+
+function deleteTask(event) {
+  event.preventDefault()
+  event.target.parentElement.remove()
+}
+
